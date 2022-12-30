a8a3854903d1b7462009e434a772880dd938e95a
Moubarak321 <benmoumass@gmail.com>
2022-12-30 13:27:54 +0100

    new

--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from app.models import Cours, Favorite
-
+from app.models import Cours, Favorite, Formateur
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
     formations = Cours.objects.all()
     favorites = Favorite.objects.all()
-    return render(request, 'app/index.html', context={"formations":formations , 'favorites':favorites})
+    formateursIn = Formateur.objects.filter(onIndex = True)
+    return render(request, 'app/index.html', context={"formations":formations , 'favorites':favorites, 'formateursIn':formateursIn})
 
 def about(request):
     formations = Cours.objects.all()
@@ -19,7 +22,8 @@ def courses(request):
 
 def trainers(request):
     formations = Cours.objects.all()
-    return render(request, 'app/trainers.html', context={"formations":formations})
+    formateurs = Formateur.objects.all()
+    return render(request, 'app/trainers.html', context={"formations":formations, 'formateurs':formateurs})
 
 def pricing(request):
     formations = Cours.objects.all()
@@ -29,9 +33,36 @@ def events(request):
     formations = Cours.objects.all()
     return render(request, 'app/events.html', context={"formations":formations})
 
+
+
 def contact(request):
     formations = Cours.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        form_data = {
+            'name':name,
+            'email':email,
+            'subject':subject,
+            'message':message,
+        }
+
+        message = '''
+        From:\n\t\t{}\n
+        Email:\n\t\t{}\n
+        Subject:\n\t\t{}\n
+        Message:\n\t\t{}\n
+        '''.format(form_data['name'], form_data['email'],form_data['subject'] ,form_data['message'] )
+        send_mail('You got a mail!', message, '', [settings.EMAIL_HOST_USER]) 
+
+        messages.success(request, "Votre message a été envoyé avec succes !!")
+        return render(request, 'app/contact.html')
     return render(request, 'app/contact.html', context={"formations":formations})
+
+
 
 def course_details(request, id):
     cours = get_object_or_404(Cours, id=id)
@@ -39,3 +70,39 @@ def course_details(request, id):
     # return render (request, 'store/detail.html', context={"product": product} )
     return render(request, 'app/course-details.html', context={"cours": cours, "formations":formations})
 
+
+# =========================
+# another way to do
+# =========================
+
+# from django.shortcuts import render, redirect
+# from .forms import ContactForm
+# from django.core.mail import send_mail, BadHeaderError
+# from django.http import HttpResponse
+
+
+# # Create your views here.
+# def homepage(request):
+# 	return render(request, "main/home.html")
+
+# def contact(request):
+# 	if request.method == 'POST':
+# 		form = ContactForm(request.POST)
+# 		if form.is_valid():
+# 			subject = "Website Inquiry" 
+# 			body = {
+# 			'first_name': form.cleaned_data['first_name'], 
+# 			'last_name': form.cleaned_data['last_name'], 
+# 			'email': form.cleaned_data['email_address'], 
+# 			'message':form.cleaned_data['message'], 
+# 			}
+# 			message = "\n".join(body.values())
+
+# 			try:
+# 				send_mail(subject, message, 'admin@example.com', ['admin@example.com']) 
+# 			except BadHeaderError:
+# 				return HttpResponse('Invalid header found.')
+# 			return redirect ("main:homepage")
+      
+# 	form = ContactForm()
+# 	return render(request, "main/contact.html", {'form':form})
--- cnib/settings.py
+++ cnib/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-s#u8hzo^+8an#_ny*9$ig129t4r1t4t)jz!%jx41$056jh27=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -125,3 +131,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+# Email setup
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_FROM = ''
+EMAIL_HOST_USER = 'devs.dudes@gmail.com'
+EMAIL_HOST_PASSWORD ='aruweomdksfiwnca'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# EMAIL_HOST = 'smtp.mailtrap.io'
+# EMAIL_HOST_USER = 'c61e71f096b365'
+# EMAIL_HOST_PASSWORD = 'e7785625273e92'
+# EMAIL_PORT = '2525'
+
+
+
+# pass:   aruweomdksfiwnca
